d67f9190da145f9c0a4de3993d3eb64611c9c6a6
KazutoEchigoya <83701557+Echigoyakazuto@users.noreply.github.com>
2024-09-03 18:59:33 +0900

    Add files via upload

---
+++ src/main/java/com/example/simplememo/SimplememoApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplememo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplememoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplememoApplication.class, args);
+	}
+
+}
